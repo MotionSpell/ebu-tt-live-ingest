77e5d8fc43c56ba467aad444a64e789096db9c86
Romain Bouqueau <romain.bouqueau.pro@gmail.com>
2021-11-10 12:08:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+This is a replacement to the EBU/BBC Live Interoperability Toolkit.
+
+The current repository contains two tools:
+- ```ws2tcp```: a minimalist WebSocket to TCP bridge. Written with node.js.
+- ```subtitle-live-ingester```: a C++ application that ingest EBU-TT-Live (WDR flavour) and outputs data for the Live Subtitle Inserter.
+
+A sample EBU-TT-Live in available in the ```sample/``` folder.
+
+```ws2tcp``` runs by default on port 9998 (Websocket) and outputs on TCP port 9999. To test:
+- ```ncat -l 9999```
+- https://websocketking.com/
+
+```subtitle-live-ingester``` is reponsible for:
+ - extracting text from EBU-TT-Live 
+ - add timings
+ - segment
+ - serialize to WebVTT and TTML dialects
+ - feed the Subtitle Live Inserter input
+
---
+++ ws2tcp/ws2tcp.js
@@ -0,0 +1,90 @@
+var logger = require('util');
+var net = require('net');
+
+var _TO_TCP = '--> ';
+var _FROM_TCP = '<-- ';
+var _TARGET = '<-> ';
+
+// WebSocket Pipe library entry point
+var WebSocketPipe = function (wsServer, debug, tcpHost, tcpPort) {
+    this.debug = debug || false;
+    this.wsPath = wsServer.path || '/';
+    this.tcpHost = tcpHost || 'localhost';
+    this.tcpPort = tcpPort;
+    log(this.debug, this.wsPath, _TARGET, this.tcpHost + ':' + this.tcpPort);
+    
+    // start handling new connections
+    var self = this;
+    wsServer.on('connection', function (webSocket) {
+        log(self.debug, self.wsPath, 'new ', webSocket.protocol);
+        var target = _newTcpClient(self, webSocket);
+        webSocket.on('message', function (msg) {
+            log(self.debug, self.wsPath, _TO_TCP, msg.toString('hex', 0, 32) + ' ' + msg.toString('ascii', 0, 32));
+            if (webSocket.protocol === 'base64') {
+                target.write(new Buffer(msg, 'base64'));
+            } else {
+                target.write(msg, 'binary');
+            }
+        });
+        webSocket.on('close', function (code, reason) {
+            log(self.debug, self.wsPath, _TARGET, "ws disconnected: " + code + '[' + reason + ']');
+            target.end();
+        });
+        webSocket.on('error', function (a) {
+            log(self.debug, self.wsPath, _TARGET, "ws error: " + a);
+            target.end();
+        });
+    });
+};
+
+function _newTcpClient(self, webSocket) {
+    var tcpClient = new net.Socket();
+    tcpClient.setKeepAlive(true);
+    tcpClient.connect(self.tcpPort, self.tcpHost, function () {
+        log(self.debug, self.wsPath, _TARGET, 'connected');
+    });
+    tcpClient.on('data', function (data) {
+        log(self.debug, self.wsPath, _FROM_TCP, data.toString('hex', 0, 32) + ' ' + data.toString('ascii', 0, 32));
+        try {
+            if (webSocket.protocol === 'base64') {
+                webSocket.send(new Buffer(data).toString('base64'));
+            } else {
+                webSocket.send(data, { binary: true });
+            }
+        } catch (e) {
+            log(self.debug, self.wsPath, _TARGET, "ws exception, closing tcp session");
+            tcpClient.end();
+        }
+    });
+    tcpClient.on('end', function () {
+        log(self.debug, self.wsPath, _TARGET, "tcp disconnected");
+        webSocket.close();
+    });
+    tcpClient.on('error', function () {
+        log(self.debug, self.wsPath, _TARGET, "tcp connection error");
+        tcpClient.end();
+        webSocket.close();
+    });
+    return tcpClient;
+};
+
+// debug request/response
+function log(debug, path, prefix, value) {
+    if (debug) {
+        try {
+            logger.debug('ws2tcp[' + path + '] ' + prefix + value.replace(/[^A-Za-z 0-9 \.,\?""!@#\$%\^&\*\(\)-_=\+;:<>\/\\\|\}\{\[\]`~]*/g, ''));
+        } catch (e) { /* ignore any exceptions */ }
+    }
+};
+
+module.exports = WebSocketPipe;
+
+// create websocket server
+var WebSocketServer = require('ws').Server;
+var server = new WebSocketServer({ port: 9998, path: '/mysocket' });
+
+// create TCP server
+var debug = true;
+var tcpServerHost = 'localhost';
+var tcpServerPort = 9999;
+var wspipe = new WebSocketPipe(server, debug, tcpServerHost, tcpServerPort);
